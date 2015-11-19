11b43a69c17ba57adfb43247e3a02aee8c22f7d2
extrant.robin <robin.extrant@iut-valence.fr>
2015-11-19 13:29:08 +0100

    adaptation of main with exceptions

--- src/fr/iutvalence/info/dut/m3105/labyrinthGame/TestLabyrinthGame.java
+++ src/fr/iutvalence/info/dut/m3105/labyrinthGame/TestLabyrinthGame.java
@@ -1,5 +1,6 @@
 package fr.iutvalence.info.dut.m3105.labyrinthGame;
 
+import java.security.InvalidParameterException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,7 +16,7 @@ public class TestLabyrinthGame
 	 * @param args
 	 *            command-line arguments (none expected here)
 	 */
-	public static void main(String[] args)
+	public static void main(String[] args) throws InvalidParameterException
 	{
 		Set<Position> blockPositions = new HashSet<Position>();
 		blockPositions.add(new Position(2,0));
@@ -25,15 +26,24 @@ public class TestLabyrinthGame
 		blockPositions.add(new Position(1,2));
 		blockPositions.add(new Position(3,2));
 		blockPositions.add(new Position(3,3));
+		
+		try
+		{
+			Labyrinth labyrinth = new LabyrinthBuilder().setWidth(4).setHeight(4)
+					.addForbiddenCellPositions(new Position(2,0))
+					.addForbiddenCellPositions(new Position(3,0))
+					.addForbiddenCellPositions(new Position(0,1))
+					.setExitPosition(new Position(3,1))
+					.getLabyrinth();
 			
-		Labyrinth labyrinth = new LabyrinthBuilder().setWidth(4).setHeight(4)
-				.addForbiddenCellPositions(new Position(2,0))
-				.addForbiddenCellPositions(new Position(3,0))
-				.addForbiddenCellPositions(new Position(0,1))
-				.setExitPosition(new Position(3,1))
-				.getLabyrinth();
+			new LabyrinthGame(labyrinth, RobotArtificialIntelligence.makeRobotArtificialIntelligence(RobotArtificialIntelligence.DBAI)).play();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
 	
-		new LabyrinthGame(labyrinth, RobotArtificialIntelligence.makeRobotArtificialIntelligence(RobotArtificialIntelligence.DBAI)).play();
+		
 
 	}
 	
